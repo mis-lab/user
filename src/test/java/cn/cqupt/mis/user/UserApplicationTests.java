216641a3847895425a306db9350d93da5263042c
Genius_DSY <genius_dsy@foxmail.com>
2018-09-20 22:20:05 +0800

    init

--- src/test/java/cn/cqupt/mis/user/UserApplicationTests.java
+++ src/test/java/cn/cqupt/mis/user/UserApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mis.usersystem;
+package cn.cqupt.mis.user;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UsersystemApplicationTests {
+public class UserApplicationTests {
 
     @Test
     public void contextLoads() {

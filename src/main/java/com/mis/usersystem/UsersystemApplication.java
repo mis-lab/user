deff3da05ff862f2165e0e6e538a1004a564d8dd
Genius_DSY <genius_dsy@foxmail.com>
2018-09-20 22:03:50 +0800

    init

---
+++ src/main/java/com/mis/usersystem/UsersystemApplication.java
@@ -0,0 +1,12 @@
+package com.mis.usersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersystemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mis/usersystem/UsersystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mis.usersystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsersystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

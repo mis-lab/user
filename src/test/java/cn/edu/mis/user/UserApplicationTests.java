25709e59ec6b3fd1e9527eb461f794e8a99f9f7f
Genius_DSY <genius_dsy@foxmail.com>
2018-09-22 12:52:13 +0800

    test: init test structure

---
+++ src/test/java/cn/edu/mis/user/UserApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.edu.mis.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = UserApplication.class)
+public class UserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

8d6b683d5b9d1eb5347ec37113f37f4d4b630726
Genius_DSY <genius_dsy@foxmail.com>
2018-09-22 12:51:36 +0800

    init:init project structure

---
+++ src/main/java/cn/edu/mis/user/UserApplication.java
@@ -0,0 +1,17 @@
+package cn.edu.mis.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author dengsiyuan
+ * @Date 2018.9.21
+ */
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}

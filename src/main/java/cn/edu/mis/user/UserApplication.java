e4645aee71b5b24419e45aa79ee6f0aaf15e1f1d
Genius_DSY <genius_dsy@foxmail.com>
2018-09-23 16:08:13 +0800

    style:modify style

--- src/main/java/cn/edu/mis/user/UserApplication.java
+++ src/main/java/cn/edu/mis/user/UserApplication.java
@@ -2,6 +2,7 @@ package cn.edu.mis.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2018.9.21
  */
 @SpringBootApplication
+@ImportResource(value = "spring-config.xml")
 public class UserApplication {
 
     public static void main(String[] args) {

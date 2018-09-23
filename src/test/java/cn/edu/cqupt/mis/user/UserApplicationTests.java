7393485df641774f49ad464accabee1fcff6f4f8
Genius_DSY <genius_dsy@foxmail.com>
2018-09-23 16:29:11 +0800

    init:add test struture

---
+++ src/test/java/cn/edu/cqupt/mis/user/UserApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.cqupt.mis.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = UserApplication.class)
+public class UserApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
